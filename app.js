bb2476fbcc74ac9241142612349451580995d43f
HannahRipley17 <d00317171@dmail.dixie.edu>
2021-04-12 14:35:40 -0600

    cleaned up some stuff, all working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-url = "ws://localhost:8080"
-// url = "ws://radiant-temple-18433.herokuapp.com/"
+// url = "ws://localhost:8080"
+url = "ws://radiant-temple-18433.herokuapp.com/"
 
 
 generateRandomComment = (senderName, message) => {
@@ -9,24 +9,6 @@ generateRandomComment = (senderName, message) => {
     };
     return socket.send(JSON.stringify(data));
 };
-
-// generateRandomPost = (title, snippet, outlet) =>  {
-//     let ampm = "am";
-//     let hours = new Date().getHours();
-//     if (hours > 12) {
-//         ampm = "pm";
-//         hours -= 12
-//     }
-//     let minutes = new Date().getMinutes();
-//     let time = hours + ":" + minutes + ampm;
-//     var data = {
-//         title: title,
-//         snippet: snippet,
-//         newsoutlet: outlet,
-//         time: time
-//     };
-//     socket.send(JSON.stringify(data));
-// };
 
 updateScroll = () => {
     var element = document.querySelector("#commentList");
@@ -89,69 +71,6 @@ var app = new Vue({
             "this is totally fake, the government just wants to control us",
             "the horse just wants the covid shot!"
         ],
-        // headers: [
-        //     "Horse loose in hospital",
-        //     "Horse runs amok in a hospital",
-        //     "Patients trampled by wild horse",
-        //     "Horse uses the elevator",
-        //     "Hospital Horse is being suspiciously quiet",
-        //     "There's a horse loose in a hospital",
-        //     "Hospital Horse fired the horse catcher",
-        //     "Hospital Horse intends to stomp patients with hooves",
-        //     "St John Hospital for Sick People asks horse to leave, horse runs away",
-        //     "Patient tries to ride Hospital Horse, falls off and breaks a leg",
-        //     "Hospital Horse is doing clerical work, helping the hospital greatly",
-        //     "Horse tries to unplug ventilator but cannot due to a lack of fingers",
-
-
-
-
-        // ],
-        // snippetStarts: [
-        //     "Hospital Horse has started to ",
-        //     "Today at 2:30pm, a horse got into the St John Hospital for Sick People and began to ",
-        //     "People are worried the horse may ",
-        //     "St John Hospital for Sick People is worried that Hospital Horse will ",
-        //     "Sources are unclear as to who let the horse ",
-        //     "Hospital Horse says 'I'm going to run towards the baby incubators and smash them with my hooves i have nice hooves and a big tail i'm a horse'. People fear the horse will ",
-        //     "No one has heard from the Hospital Horse in a while. Everyone fears the horse will ",
-        //     "The horse catcher was supposed to stop the horse but the horse fired him. Now there is no one to stop the horse's determination to "
-        // ],
-        // verbs: [
-        //     "run amok",
-        //     "rampage",
-        //     "destroy the hospital",
-        //     "go wild",
-        //     "run wild",
-        //     "trample",
-        //     "eat hospital equipment",
-        //     "knock everything over",
-        //     "kick the patients",
-        //     "pee on the doctors",
-        //     "catch COVID",
-        //     "kick everything",
-        //     "knock over the IV bags",
-        //     "commit unspeakable acts of violence",
-        //     "is actually a spy for bad guys",
-        //     "will try to do surgery"
-        // ],
-        // snippetEnds: [
-        //     " in a hospital.",
-        //     " at the St John Hospital for Sick People.",
-        //     ", squishing all the patients.",
-        //     ", killing 37 people.",
-        //     ". No one knows how the horse got into the hospital.",
-        //     " and harm patients",
-        //     ". No one seems to be doing anything about it."
-
-        // ],
-        // outlets: [
-        //     "CMM News",
-        //     "Jackal News",
-        //     "NCB News",
-        //     "XYZ News",
-        // ]
-    
     },
 
     
@@ -179,6 +98,7 @@ var app = new Vue({
                     this.messageList.splice(0, 1);
                 }
             }
+            console.log(this.newsList);
             
             // fun fact - data from a websocket is always a string but it could be a json string
             // gotta JSON.stringify like in sendMessage and then JSON.parse it when you get it back
@@ -202,28 +122,7 @@ var app = new Vue({
             this.showWholeList = false;
             this.showShortList = true;
         },
-        
-        
-        
 
-        // makePost: function() {
-        //     let headers = [...this.headers];
-        //     let snippetStarts = [...this.snippetStarts];
-        //     let verbs = [...this.verbs];
-        //     let snippetEnds = [...this.snippetEnds];
-        //     let outlets = [...this.outlets];
-
-        //     let randomTime = Math.floor(Math.random() * 10000);
-            
-        //     setInterval(function() {
-
-        //         let title = headers[Math.floor(Math.random() * headers.length)];
-        //         let snippet = snippetStarts[Math.floor(Math.random() * snippetStarts.length)] + verbs[Math.floor(Math.random() * verbs.length)] + snippetEnds[Math.floor(Math.random() * snippetEnds.length)];
-        //         let outlet = outlets[Math.floor(Math.random() * outlets.length)];
-            
-        //         generateRandomPost(title, snippet, outlet);
-        //     }, randomTime);
-        // },
         makeComment: function() {
             let comments = [...this.comments];
             let randomTime = 4000;
@@ -256,7 +155,6 @@ var app = new Vue({
             this.showChatBubble = true;
         };
         this.connectSocket();
-        //this.makePost();
         this.makeComment();
 
     }
