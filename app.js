e66e6e15a2fdb99e9cf244133897d2a17c5da395
HannahRipley17 <d00317171@dmail.dixie.edu>
2021-04-12 14:21:26 -0600

    moved post generator to server

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 url = "ws://localhost:8080"
+// url = "ws://radiant-temple-18433.herokuapp.com/"
+
 
 generateRandomComment = (senderName, message) => {
     var data = {
@@ -8,23 +10,23 @@ generateRandomComment = (senderName, message) => {
     return socket.send(JSON.stringify(data));
 };
 
-generateRandomPost = (title, snippet, outlet) =>  {
-    let ampm = "am";
-    let hours = new Date().getHours();
-    if (hours > 12) {
-        ampm = "pm";
-        hours -= 12
-    }
-    let minutes = new Date().getMinutes();
-    let time = hours + ":" + minutes + ampm;
-    var data = {
-        title: title,
-        snippet: snippet,
-        newsoutlet: outlet,
-        time: time
-    };
-    socket.send(JSON.stringify(data));
-};
+// generateRandomPost = (title, snippet, outlet) =>  {
+//     let ampm = "am";
+//     let hours = new Date().getHours();
+//     if (hours > 12) {
+//         ampm = "pm";
+//         hours -= 12
+//     }
+//     let minutes = new Date().getMinutes();
+//     let time = hours + ":" + minutes + ampm;
+//     var data = {
+//         title: title,
+//         snippet: snippet,
+//         newsoutlet: outlet,
+//         time: time
+//     };
+//     socket.send(JSON.stringify(data));
+// };
 
 updateScroll = () => {
     var element = document.querySelector("#commentList");
@@ -42,6 +44,9 @@ var app = new Vue({
         socket: null,
         showShortList: true,
         showWholeList: false,
+        commentBoxOpen: true,
+        showChatBubble: false,
+        showXButton: false,
         newMessage: '',
         senderName: 'user' + Math.floor(Math.random() * 1000),
         messageList: [
@@ -78,63 +83,83 @@ var app = new Vue({
             "we're well beyond that!",
             "maybe the horse catcher will catch the horse",
             "the horse has fired the horse catcher!",
+            "why isn't anybody stopping him?!?!",
+            "oh no the poor thing :(",
+            "umm... hello! this is a big problem",
+            "this is totally fake, the government just wants to control us",
+            "the horse just wants the covid shot!"
         ],
-        headers: [
-            "Horse loose in hospital",
-            "Horse runs amok in a hospital",
-            "Patients trampled by wild horse",
-            "Horse uses the elevator",
-            "Hospital Horse is being suspiciously quiet",
-            "There's a horse loose in a hospital",
-            "Hospital Horse fired the horse catcher",
-            "Hospital Horse intends to stomp patients with hooves",
-            "St John Hospital for Sick People asks horse to leave, horse runs away",
-        ],
-        snippetStarts: [
-            "Hospital Horse has started to ",
-            "Today at 2:30pm, a horse got into the St John Hospital for Sick People and began to ",
-            "People are worried the horse may ",
-            "St John Hospital for Sick People is worried that Hospital Horse will ",
-            "Sources are unclear as to who let the horse ",
-            "Hospital Horse says 'I'm going to run towards the baby incubators and smash them with my hooves i have nice hooves and a big tail i'm a horse'. People fear the horse will ",
-            "No one has heard from the Hospital Horse in a while. Everyone fears the horse will ",
-            "The horse catcher was supposed to stop the horse but the horse fired him. Now there is no one to stop the horse's determination to "
-        ],
-        verbs: [
-            "run amok",
-            "rampage",
-            "destroy the hospital",
-            "go wild",
-            "run wild",
-            "trample",
-            "eat hospital equipment",
-            "knock everything over",
-            "kick the patients",
-            "pee on the doctors",
-            "catch COVID",
-            "kick everything",
-            "knock over the IV bags"
-        ],
-        snippetEnds: [
-            " in a hospital.",
-            " at the St John Hospital for Sick People.",
-            ", squishing all the patients.",
-            ", killing 37 people.",
-            ". No one knows how the horse got into the hospital.",
-            " and harm patients",
-            ". No one seems to be doing anything about it."
+        // headers: [
+        //     "Horse loose in hospital",
+        //     "Horse runs amok in a hospital",
+        //     "Patients trampled by wild horse",
+        //     "Horse uses the elevator",
+        //     "Hospital Horse is being suspiciously quiet",
+        //     "There's a horse loose in a hospital",
+        //     "Hospital Horse fired the horse catcher",
+        //     "Hospital Horse intends to stomp patients with hooves",
+        //     "St John Hospital for Sick People asks horse to leave, horse runs away",
+        //     "Patient tries to ride Hospital Horse, falls off and breaks a leg",
+        //     "Hospital Horse is doing clerical work, helping the hospital greatly",
+        //     "Horse tries to unplug ventilator but cannot due to a lack of fingers",
 
-        ],
-        outlets: [
-            "CMM News",
-            "Jackal News",
-            "NCB News",
-            "XYZ News",
-        ]
+
+
+
+        // ],
+        // snippetStarts: [
+        //     "Hospital Horse has started to ",
+        //     "Today at 2:30pm, a horse got into the St John Hospital for Sick People and began to ",
+        //     "People are worried the horse may ",
+        //     "St John Hospital for Sick People is worried that Hospital Horse will ",
+        //     "Sources are unclear as to who let the horse ",
+        //     "Hospital Horse says 'I'm going to run towards the baby incubators and smash them with my hooves i have nice hooves and a big tail i'm a horse'. People fear the horse will ",
+        //     "No one has heard from the Hospital Horse in a while. Everyone fears the horse will ",
+        //     "The horse catcher was supposed to stop the horse but the horse fired him. Now there is no one to stop the horse's determination to "
+        // ],
+        // verbs: [
+        //     "run amok",
+        //     "rampage",
+        //     "destroy the hospital",
+        //     "go wild",
+        //     "run wild",
+        //     "trample",
+        //     "eat hospital equipment",
+        //     "knock everything over",
+        //     "kick the patients",
+        //     "pee on the doctors",
+        //     "catch COVID",
+        //     "kick everything",
+        //     "knock over the IV bags",
+        //     "commit unspeakable acts of violence",
+        //     "is actually a spy for bad guys",
+        //     "will try to do surgery"
+        // ],
+        // snippetEnds: [
+        //     " in a hospital.",
+        //     " at the St John Hospital for Sick People.",
+        //     ", squishing all the patients.",
+        //     ", killing 37 people.",
+        //     ". No one knows how the horse got into the hospital.",
+        //     " and harm patients",
+        //     ". No one seems to be doing anything about it."
+
+        // ],
+        // outlets: [
+        //     "CMM News",
+        //     "Jackal News",
+        //     "NCB News",
+        //     "XYZ News",
+        // ]
+    
     },
 
     
     methods: {
+        onResize: function(event) {
+            console.log('window has been resized', event) 
+        },
+
         connectSocket: function() {
             console.log("gonna connect a socket");
             socket = new WebSocket(url); // connect to the server
@@ -146,7 +171,6 @@ var app = new Vue({
         getMessage: function(event) {
             var data = JSON.parse(event.data);
             if (data.title) {
-                console.log(data);
                 this.newsList.unshift(data);
 
             } else {
@@ -182,24 +206,24 @@ var app = new Vue({
         
         
 
-        makePost: function() {
-            let headers = [...this.headers];
-            let snippetStarts = [...this.snippetStarts];
-            let verbs = [...this.verbs];
-            let snippetEnds = [...this.snippetEnds];
-            let outlets = [...this.outlets];
+        // makePost: function() {
+        //     let headers = [...this.headers];
+        //     let snippetStarts = [...this.snippetStarts];
+        //     let verbs = [...this.verbs];
+        //     let snippetEnds = [...this.snippetEnds];
+        //     let outlets = [...this.outlets];
 
-            let randomTime = Math.floor(Math.random() * 10000);
+        //     let randomTime = Math.floor(Math.random() * 10000);
             
-            setInterval(function() {
+        //     setInterval(function() {
 
-                let title = headers[Math.floor(Math.random() * headers.length)];
-                let snippet = snippetStarts[Math.floor(Math.random() * snippetStarts.length)] + verbs[Math.floor(Math.random() * verbs.length)] + snippetEnds[Math.floor(Math.random() * snippetEnds.length)];
-                let outlet = outlets[Math.floor(Math.random() * outlets.length)];
+        //         let title = headers[Math.floor(Math.random() * headers.length)];
+        //         let snippet = snippetStarts[Math.floor(Math.random() * snippetStarts.length)] + verbs[Math.floor(Math.random() * verbs.length)] + snippetEnds[Math.floor(Math.random() * snippetEnds.length)];
+        //         let outlet = outlets[Math.floor(Math.random() * outlets.length)];
             
-                generateRandomPost(title, snippet, outlet);
-            }, randomTime);
-        },
+        //         generateRandomPost(title, snippet, outlet);
+        //     }, randomTime);
+        // },
         makeComment: function() {
             let comments = [...this.comments];
             let randomTime = 4000;
@@ -211,11 +235,28 @@ var app = new Vue({
                 updateScroll();
             }, randomTime);
         },
+
+        showChat: function () {
+            this.commentBoxOpen = true;
+            this.showChatBubble = false;
+            this.showXButton = true;
+        },
+        closeChat: function () {
+            this.commentBoxOpen = false;
+            this.showChatBubble = true;
+            this.showXButton = false;
+        },
+        
+
     },
     created: function() {
         console.log("loaded Vue :)");
+        if (window.innerWidth < 500) {
+            this.commentBoxOpen = false;
+            this.showChatBubble = true;
+        };
         this.connectSocket();
-        this.makePost();
+        //this.makePost();
         this.makeComment();
 
     }
--- server.js
+++ server.js
@@ -19,6 +19,96 @@ var server = app.listen(port, function () {
     console.log("Server is listening on port", port)
 });
 
+let headers = [
+    "Horse loose in hospital",
+    "Horse runs amok in a hospital",
+    "Patients trampled by wild horse",
+    "Horse uses the elevator",
+    "Hospital Horse is being suspiciously quiet",
+    "There's a horse loose in a hospital",
+    "Hospital Horse fired the horse catcher",
+    "Hospital Horse intends to stomp patients with hooves",
+    "St John Hospital for Sick People asks horse to leave, horse runs away",
+    "Patient tries to ride Hospital Horse, falls off and breaks a leg",
+    "Hospital Horse is doing clerical work, helping the hospital greatly",
+    "Horse tries to unplug ventilator but cannot due to a lack of fingers",
+];
+
+let snippetStarts = [
+    "Hospital Horse has started to ",
+    "Today at 2:30pm, a horse got into the St John Hospital for Sick People and began to ",
+    "People are worried the horse may ",
+    "St John Hospital for Sick People is worried that Hospital Horse will ",
+    "Sources are unclear as to who let the horse ",
+    "Hospital Horse says 'I'm going to run towards the baby incubators and smash them with my hooves i have nice hooves and a big tail i'm a horse'. People fear the horse will ",
+    "No one has heard from the Hospital Horse in a while. Everyone fears the horse will ",
+    "The horse catcher was supposed to stop the horse but the horse fired him. Now there is no one to stop the horse's determination to "
+];
+
+let verbs = [
+    "run amok",
+    "rampage",
+    "destroy the hospital",
+    "go wild",
+    "run wild",
+    "trample",
+    "eat hospital equipment",
+    "knock everything over",
+    "kick the patients",
+    "pee on the doctors",
+    "catch COVID",
+    "kick everything",
+    "knock over the IV bags",
+    "commit unspeakable acts of violence",
+    "is actually a spy for bad guys",
+    "will try to do surgery"
+];
+
+let snippetEnds = [
+    " in a hospital.",
+    " at the St John Hospital for Sick People.",
+    ", squishing all the patients.",
+    ", killing 37 people.",
+    ". No one knows how the horse got into the hospital.",
+    " and harm patients",
+    ". No one seems to be doing anything about it."
+
+];
+
+let outlets = [
+    "CMM News",
+    "Jackal News",
+    "NCB News",
+    "XYZ News",
+];
+    
+setInterval(() => {
+    let title = headers[Math.floor(Math.random() * headers.length)];
+    let snippet = snippetStarts[Math.floor(Math.random() * snippetStarts.length)] + verbs[Math.floor(Math.random() * verbs.length)] + snippetEnds[Math.floor(Math.random() * snippetEnds.length)];
+    let outlet = outlets[Math.floor(Math.random() * outlets.length)];
+
+    generateRandomPost(title, snippet, outlet);
+}, Math.floor(Math.random() * 10000));
+
+
+generateRandomPost = (title, snippet, outlet) =>  {
+    let ampm = "am";
+    let hours = new Date().getHours();
+    if (hours > 12) {
+        ampm = "pm";
+        hours -= 12
+    }
+    let minutes = new Date().getMinutes();
+    let time = hours + ":" + minutes + ampm;
+    var data = {
+        title: title,
+        snippet: snippet,
+        newsoutlet: outlet,
+        time: time
+    };
+    broadcastToEveryone(JSON.stringify(data));
+};
+
 
 // WebSocket stuff
 const wss = new WebSocket.Server({ server: server });
@@ -31,6 +121,8 @@ function broadcastToEveryone(data) {
     });
 };
 
+
+
 function sendData(client, data) {
     client.send(JSON.stringify(data));
 }
@@ -38,7 +130,6 @@ function sendData(client, data) {
 wss.on('connection', function (newClient) { // when a new incoming client first connects
     console.log("a new client just connected to the server!");
     newClient.on('message', function (data) { // when the new client sends something to the server
-        console.log("a client just sent a message to the server: ", data);
         broadcastToEveryone(data);
     });
 });
